757d9027b4c776bf0fa9f58b8a5822dc0f3e4421
Santiago Avalloni <santiago.avalloni@ug.uchile.cl>
2026-05-05 12:31:41 -0400

    Se agrega a main testeo de 5.1

--- src/t1.c
+++ src/t1.c
@@ -2,25 +2,44 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "../include/rtree.h"
 
 
 int main() {
-  FILE *datos = fopen("./datos/random.bin", "rb"); 
-  if (datos==NULL) {
-    perror("Error abriendo el archivo de datos");
+  FILE *datos_rand = fopen("./datos/random.bin", "rb"); 
+  if (datos_rand==NULL) {
+    perror("Error abriendo el archivo de datos aleatorios\n");
     return 1;
   }
 
-  FILE *vector = fopen("./vectores/vec_nodos_rand.bin", "w");
-  if (vector==NULL) {
-    perror("Error abriendo el archivo de salida");
-    return 1;
-  }
-  
+  for(int i = 15; i<=24; i++) {
+    remove("./vectores/vec_nodos_rand.bin");
 
-  unsigned long N = 1<<15;
-  bulkLoading(N, datos, vector, nearestX);
+
+    FILE *vector = fopen("./vectores/vec_nodos_rand.bin", "w");
+    if (vector==NULL) {
+      perror("Error abriendo el archivo de salida rand NX\n");
+      return 1;
+    }
+
+    unsigned long N = (unsigned long)1<<i;
+
+
+    clock_t start, end;
+    
+    
+    double cpu_time_used;
+    
+    start = clock();
+    bulkLoading(N, datos_rand, vector, nearestX);
+    end = clock();
+
+    fclose(vector);
+    
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Tomó %f segundos en ejecutarse para N=2^%d\n", cpu_time_used, i);
+  }
   
   return 0;
 }
