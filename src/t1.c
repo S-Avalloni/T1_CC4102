95e135d75aa503046b027b795cd7a023fe53e69c
Santiago Avalloni <santiago.avalloni@ug.uchile.cl>
2026-05-06 00:38:17 -0400

    Se completa 5.1 de la tarea y se escribe en un archivo

--- src/t1.c
+++ src/t1.c
@@ -15,7 +15,6 @@ int timeTest(const char *infile, const char *outfile, BulkFunction bulkFunction)
   }
 
   for(int i = 15; i<=24; i++) {
-    fseek(datos, 0, SEEK_SET);
     remove(outfile);
 
 
@@ -41,8 +40,11 @@ int timeTest(const char *infile, const char *outfile, BulkFunction bulkFunction)
     
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Tomó %f segundos en ejecutarse para N=2^%d\n", cpu_time_used, i);
+    fseek(datos, 0, SEEK_SET);
   }
   
+  fclose(datos);
+
   return 0;
 
 }
@@ -53,21 +55,23 @@ int main() {
   const char *datos_random = "./datos/random.bin";
   const char *datos_eur = "./datos/europa.bin";
 
-  const char *vector_random = "./vectores/v_random.bin";
-  const char *vector_eur = "./vectores/v_europa.bin";
+  const char *vector_ran_nx = "./vectores/v_random_nx.bin";
+  const char *vector_eur_nx = "./vectores/v_europa_nx.bin";
+  const char *vector_ran_st = "./vectores/v_random_st.bin";
+  const char *vector_eur_st = "./vectores/v_europa_st.bin";
   
   
-  printf("Test para datos random y NearestX\n");
-  timeTest(datos_random, vector_random, nearestX);
+  printf("\nTest para datos random y NearestX\n");
+  timeTest(datos_random, vector_ran_nx, nearestX);
 
-  printf("Test para datos europa y NearestX\n");
-  timeTest(datos_random, vector_eur, nearestX);
+  printf("\nTest para datos europa y NearestX\n");
+  timeTest(datos_eur, vector_eur_nx, nearestX);
 
-  // printf("Test para datos random y SortTileRecursive\n");
-  // timeTest(datos_random, vector_random, sortTileRecursive);
+  printf("\nTest para datos random y SortTileRecursive\n");
+  timeTest(datos_random, vector_ran_st, sortTileRecursive);
 
-  // printf("Test para datos europa y SortTileRecursive\n");
-  // timeTest(datos_random, vector_eur, sortTileRecursive);
+  printf("\nTest para datos europa y SortTileRecursive\n");
+  timeTest(datos_eur, vector_eur_st, sortTileRecursive);
 
   return 0;
 }
